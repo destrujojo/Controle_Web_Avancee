6ebb9bdc55313e4117d6c5275f04d748a2c226e5
destrujojo <destrujojo@gmail.com>
2023-10-11 15:45:16 +0200

    Dynamique 8 personnages

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("Let's go !");
+
+AppelleApi(8);
+
+async function AppelleApi(nbrlimite) {
+  const data = await fetch("https://hp-api.onrender.com/api/characters")
+    .then((response) => response.json())
+    .catch((error) => alert("Erreur : " + error));
+  console.log(data);
+
+  for (let i = 0; i < nbrlimite; i++) {
+    let newDiv = document.createElement("div");
+    let maDiv = document.querySelector(".img_caracter_list");
+
+    newDiv.classList.add("caracter_detail", data[i].id, data[i].house);
+
+    newDiv.innerHTML = `<div class="img_caracter"><img src="${data[i].image}" alt="" /></div>
+              <p>${data[i].name}</p>`;
+
+    maDiv.appendChild(newDiv);
+  }
+}
