553341828fc5abd66d497fbbc5c3aa26b39906d5
destrujojo <destrujojo@gmail.com>
2023-10-11 16:31:06 +0200

    Mise en place click sur maison pour filtre en cours loin d'etre finie

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ console.log("Let's go !");
 
 AppelleApi(8);
 
+let tableauMaison = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+
+let divHouse = document.querySelector(".housse_bloc");
+
+divHouse.addEventListener("click", () => (e) => housseselect(e.target));
+
 async function AppelleApi(nbrlimite) {
   const data = await fetch("https://hp-api.onrender.com/api/characters")
     .then((response) => response.json())
@@ -18,5 +24,33 @@ async function AppelleApi(nbrlimite) {
               <p>${data[i].name}</p>`;
 
     maDiv.appendChild(newDiv);
+  }
+}
+
+function housseselect(valeur) {
+  console.log(valeur);
+}
+
+function creationLogoMaison() {
+  for (let i = 0; i < 4; i++) {
+    let newDiv = document.createElement("div");
+    let maDiv = document.querySelector(".housse_bloc");
+
+    newDiv.addEventListener("click", (e) => housseselect(e.target));
+
+    switch (tableauMaison[i]) {
+      case Gryffindor:
+        newDiv.innerHTML = `<img src="./images/logo/Gryffindor.png" alt="" />`;
+        break;
+      case Hufflepuff:
+        newDiv.innerHTML = `<img src="./images/logo/Hufflepuff.png" alt="" />`;
+        break;
+      case Ravenclaw:
+        newDiv.innerHTML = `<img src="./images/logo/Ravenclaw.png" alt="" />`;
+        break;
+      case Slytherin:
+        newDiv.innerHTML = `<img src="./images/logo/Slytherin.png" alt="" />`;
+        break;
+    }
   }
 }
